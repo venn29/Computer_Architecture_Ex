c04a9e9ecb4651c61e0e75401f3af05a70f721c8
venn29 <37021739+venn29@users.noreply.github.com>
2019-05-16 23:32:10 +0800

    FIFO big fail?

--- EX3_Cache/LRU/CacheSrcCode/cache.sv
+++ EX3_Cache/LRU/CacheSrcCode/cache.sv
@@ -19,7 +19,7 @@ localparam MEM_ADDR_LEN    = TAG_ADDR_LEN + SET_ADDR_LEN ; // 计算主存地址
 localparam UNUSED_ADDR_LEN = 32 - TAG_ADDR_LEN - SET_ADDR_LEN - LINE_ADDR_LEN - 2 ;       // 计算未使用的地址的长�?
 
 localparam LINE_SIZE       = 1 << LINE_ADDR_LEN  ;         // 计算 line �? word 的数量，�? 2^LINE_ADDR_LEN 个word �? line
-localparam SET_SIZE        = 1 << SET_ADDR_LEN   ;         // 计算?共有多少组，2^SET_ADDR_LEN 个组
+localparam SET_SIZE        = 1 << SET_ADDR_LEN   ;         // 计算�?共有多少组，�? 2^SET_ADDR_LEN 个组
 
 reg [            31:0] cache_mem    [SET_SIZE][WAY_CNT][LINE_SIZE]; // SET_SIZE个line，每个line有LINE_SIZE个word
 reg [TAG_ADDR_LEN-1:0] cache_tags   [SET_SIZE][WAY_CNT];            // SET_SIZE*WAY_CNT个TAG
@@ -48,9 +48,10 @@ wire mem_gnt;      // 主存响应读写的握手信�?
 
 assign {unused_addr, tag_addr, set_addr, line_addr, word_addr} = addr;  // 拆分 32bit ADDR
 
-reg  [5:0] SwichOut [SET_SIZE];       //指示本组中即将被换出的路,FIFO,只需要不断循环就可以�?
+reg  [31:0] Vist [SET_SIZE][WAY_CNT];       //指示每一路的访问情况，当被访问或者换入的时候，置为0，否则随着时钟周期增加，增加到最大值就不变了
 reg cache_hit = 1'b0;
-reg [5:0] HIT_WAY;     //命中的是第几�?
+reg [5:0] HIT_WAY;     //命中的是第几行?
+reg [5:0] OutWay;       //被换出的行
 always @ (*)         // 判断 输入的address 是否�? cache 中命�?
 begin             
     for(integer i=0;i<WAY_CNT;i++)
@@ -75,11 +76,10 @@ always @ (posedge clk or posedge rst) begin     // ?? cache ???
             begin
                 dirty[i][j] = 1'b0;
                 valid[i][j] = 1'b0;
+                Vist[i][j]=  32'hffff;
             end
         end
 
-        for(integer i=0; i<SET_SIZE; i++) 
-            SwichOut[i]=0;
 
         for(integer k=0; k<LINE_SIZE; k++)
             mem_wr_line[k] <= 0;
@@ -88,7 +88,7 @@ always @ (posedge clk or posedge rst) begin     // ?? cache ???
         rd_data <= 0;
     end 
     else begin
-        case(cache_stat)
+        case(cache_stat)            //只有在IDLE分支才会去更新所有一个组的vist数组，在其他情况下不会
         IDLE:       begin
                         if( cache_hit ) begin
                             if(rd_req) begin    // 如果cache命中，并且是读请求，
@@ -99,7 +99,7 @@ always @ (posedge clk or posedge rst) begin     // ?? cache ???
                             end 
                         end else begin
                             if(wr_req | rd_req) begin   // 如果 cache 未命中，并且有读写请求，则需要进行换�?
-                                if( valid[set_addr][SwichOut[set_addr]] & dirty[set_addr][SwichOut[set_addr]]  ) // 如果 要换入的cache line 本来有效，且脏，则需要先将它换出
+                                /*if( valid[set_addr][SwichOut[set_addr]] & dirty[set_addr][SwichOut[set_addr]]  ) // 如果 要换入的cache line 本来有效，且脏，则需要先将它换出
                                 begin    
                                     cache_stat  <= SWAP_OUT;
                                     mem_wr_addr <= { cache_tags[set_addr][SwichOut[set_addr]], set_addr };
@@ -114,7 +114,24 @@ always @ (posedge clk or posedge rst) begin     // ?? cache ???
                                         SwichOut[set_addr]=SwichOut[set_addr]+1'b1;             //循环递增每一组的SwitchOut
                                     else
                                         SwichOut[set_addr]=0;
+                                end*/
+
+                                //为了达成目的，必须阻塞
+                                for(int j=0;j<WAY_CNT;j++)
+                                begin
+                                    if( !valid[set_addr][j])        //发现无效块，直接选择
+                                    begin
+                                        OutWay=j;
+                                        break;
+                                    end
+                                    else if(Vist[set_addr][j]>Vist[set_addr][OutWay])
+                                    begin
+                                        OutWay=j
+                                        break;
+                                    end
                                 end
+                                if(valid[set_addr][OutWay] & dirty[set_addr][OutWay])       //
+
                                 {mem_rd_tag_addr, mem_rd_set_addr} <= {tag_addr, set_addr};
                             end
                         end
